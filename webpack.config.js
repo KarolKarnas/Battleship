51e85b1d703e9e67f3e32fd4f557dc8b831ab245
KarolKarnas <karol.karnas@gmail.com>
2023-04-07 12:25:09 +0200

    Create basic setup and files
    setup:
    - Webpack (use 'npx webpack --watch'))
    - JEST
    - Babel (use import in JEST)
    -

---
+++ webpack.config.js
@@ -0,0 +1,28 @@
+const path = require('path');
+
+module.exports = {
+	mode: 'development',
+	entry: './src/index.js',
+	output: {
+		filename: 'main.js',
+		path: path.resolve(__dirname, 'dist'),
+    // environment: {
+    //   arrowFunction: false,
+    // },
+	},
+  devtool: 'source-map',
+	module: {
+		rules: [
+			{
+				test: /\.m?js$/,
+        exclude: /node_modules/,
+				use: {
+					loader: 'babel-loader',
+					options: {
+						presets: [['@babel/preset-env', { targets: 'defaults' }]],
+					},
+				},
+			},
+		],
+	},
+};
