747b7f9e781c74dd011579609d649da65ba915c5
KarolKarnas <karol.karnas@gmail.com>
2023-04-12 16:51:59 +0200

    Refactor renderGameboard(), mod styles
    Render gameboard for player and for ai;
    Populate gameboard with ships for player and ai with predetermined coordinates;

--- src/modules/Game.js
+++ src/modules/Game.js
@@ -10,9 +10,34 @@ class Game {
 		this.player = new Player();
 		this.ai = new Player();
 
-        this.gameboardPlayer.placeShip([0,0], 'x', 1)
-        this.gameboardPlayer.placeShip([6,9], 'x', 4)
+        this.placeShipsPlayer()
+        this.placeShipsAi()
 	}
+
+    placeShipsPlayer() {
+        this.gameboardPlayer.placeShip([0,0], 'x', 1)
+        this.gameboardPlayer.placeShip([1,4], 'x', 1)
+        this.gameboardPlayer.placeShip([6,7], 'x', 1)
+        this.gameboardPlayer.placeShip([9,0], 'x', 1)
+        this.gameboardPlayer.placeShip([1,2], 'x', 2)
+        this.gameboardPlayer.placeShip([6,4], 'y', 2)
+        this.gameboardPlayer.placeShip([1,7], 'y', 2)
+        this.gameboardPlayer.placeShip([6,2], 'x', 3)
+        this.gameboardPlayer.placeShip([4,4], 'y', 3)
+        this.gameboardPlayer.placeShip([6,9], 'x', 4)
+    }
+    placeShipsAi() {
+        this.gameboardAi.placeShip([1,0], 'x', 1)
+        this.gameboardAi.placeShip([0,4], 'x', 1)
+        this.gameboardAi.placeShip([5,7], 'x', 1)
+        this.gameboardAi.placeShip([8,0], 'x', 1)
+        this.gameboardAi.placeShip([3,0], 'y', 2)
+        this.gameboardAi.placeShip([7,4], 'y', 2)
+        this.gameboardAi.placeShip([0,7], 'y', 2)
+        this.gameboardAi.placeShip([6,2], 'x', 3)
+        this.gameboardAi.placeShip([3,4], 'x', 3)
+        this.gameboardAi.placeShip([4,9], 'x', 4)
+    }
 
 	playerTurn() {
 		this.player.attack(gameboardAi, [1, 1]);
--- src/modules/Ui.js
+++ src/modules/Ui.js
@@ -1,12 +1,15 @@
 class Ui {
 	constructor(game) {
+        const playerGameboardElement = document.getElementById('player-gameboard');
+        const aiGameboardElement = document.getElementById('ai-gameboard');
 		this.game = game;
-		this.renderGameboard(this.game.gameboardPlayer.board);
+		this.renderGameboard(this.game.gameboardPlayer.board, playerGameboardElement);
+		this.renderGameboard(this.game.gameboardAi.board, aiGameboardElement);
 	}
 
-renderGameboard(gameboard) {
+renderGameboard(gameboard, gameboardElement) {
     console.log(gameboard);
-    const gameboardElement = document.getElementById('player-gameboard');
+    
     for (let i = 0; i < 10; i++) {
         for (let j = 0; j < 10; j++) {
             const divField = document.createElement('div');
